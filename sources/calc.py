07f80a16ebeaa0cc78b32aae208e8376e4d09dae
Hani Hammadhe <HaniHammadeh@users.noreply.github.com>
2020-04-02 09:40:11 +0400

    Update calc.py

--- sources/calc.py
+++ sources/calc.py
@@ -4,6 +4,7 @@ them together. If either value is a string (or both of them are) 'add2' ensures
 they are both strings, thereby resulting in a concatenated result.
 NOTE: If a value submitted to the 'add2' function is a float, it must be done so
 in quotes (i.e. as a string).
+test line
 '''
 
 # If 'value' is not an integer, convert it to a float and failing that, a string.
